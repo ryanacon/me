ba6ac85b94e729fa145583ac1acf65567a6666dd
Roberto Yanacon <robertoyanacon@gmail.com>
2019-10-30 22:13:48 -0300

    agregado de sticky nav

--- script.js
+++ script.js
@@ -1,19 +1,39 @@
 // pure javascript
 // sticky nav
 // When the user scrolls the page, execute myFunction
-window.onscroll = function () { myFunction() };
+window.onscroll = function () { 
+    
+    myFunction() 
+};
 
 // Get the navbar
-var navbar = document.getElementById("navbar");
+let navbar = document.getElementById("navbar");
+let technology = document.getElementById("technology");
 
 // Get the offset position of the navbar
 var sticky = navbar.offsetTop;
 
 // Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
 function myFunction() {
-    if (window.pageYOffset >= sticky) {
-        navbar.classList.add("sticky")
-    } else {
-        navbar.classList.remove("sticky");
+    var divOffset = offset(technology);
+    console.log(divOffset.left)
+    console.log(window.pageXOffset)
+    offset(technology)
+    if (window.pageYOffset > 0) {
+        navbar.classList.add("sticky");
+        navbar.classList.add("nav-color");
     }
+   
+    else {
+        navbar.classList.remove("sticky");
+        navbar.classList.remove("nav-color");
+    }
+}
+
+//   get position of element
+function offset(el) {
+    var rect = el.getBoundingClientRect(),
+    scrollLeft = window.pageXOffset || document.documentElement.scrollLeft,
+    scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+    return { top: rect.top + scrollTop, left: rect.left + scrollLeft }
 }
