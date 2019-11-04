5693f8e42f083c242270683e0d2667ebb26cfa3f
Roberto Yanacon <robertoyanacon@gmail.com>
2019-11-04 10:59:53 -0300

    incorporacion de nuevos section

--- script.js
+++ script.js
@@ -1,15 +1,50 @@
-$(document).ready(()=>{
-    $('#aboutme')
-    $('#technology').hide()
+// $(document).ready(() => {
+//     $('#aboutme')
+//     $('#intro').css({opacity: 0})
+//     $('#technology').css({opacity: 0})
 
-    $(window).scroll(()=>{
-        let y = window.scrollY;
-        let e = $('#technology').position().top
-        if(y+e > e){
-            $('#technology').fadeIn('slow')
-        }
-        else {
-            $('#technology').fadeOut('slow')
-        }
-    })
-})
+//     $(window).scroll(() => {
+//         hideAndShowElements()
+//         fixedNav()
+//     })
+
+//     fixedNav = () =>{
+//         let y = window.scrollY;
+//         if(y>0){
+//             $('#navbar').addClass("sticky");
+//             $('#navbar').animate({
+                
+//             },1000);
+//         }
+//         else {
+//             $('#navbar').removeClass("sticky");
+//             $('#navbar').removeClass("nav-color");
+//         }
+//     }
+
+//     hideAndShowElements = () => {
+
+//         let scrolledwin = window.scrollY;
+//         let introsection = $('#intro').position().top;
+//         let technosection = $('#technology').position().top;
+        
+       
+//        console.log(technosection)
+//         // console.log(scrolledwin + technosection)
+//         // console.log(introsection)
+//         // console.log(scrolledwin)
+//         console.log(scrolledwin)
+
+//         if (scrolledwin + introsection > introsection) {
+//         $('#intro').css({opacity: 1})
+//         } 
+//         if(scrolledwin + technosection > technosection){
+//             $('#technology').css({opacity: 1})
+//         }
+       
+       
+        
+
+//     }
+
+// })
